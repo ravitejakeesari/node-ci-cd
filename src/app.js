4238f44c12a802cf9618a14da9116dcab83b2abd
Ravi Teja Keesari <teja.keesari@gmail.com>
2019-12-05 14:21:08 +0530

    added new test get api

--- src/app.js
+++ src/app.js
@@ -4,3 +4,8 @@ var app = express();
 app.listen(9000, () => {
     console.log('Server listening on port 9000');
 })
+
+app.get('/', (req, res) => {
+    console.log('get api');
+    res.status(200).send('Test Api')
+})
